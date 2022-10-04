0837fded3b1d3fe122c98755c4dc1d1fdd512eba
sreerang k <sreerangk77@gmail.com>
2022-10-04 18:46:03 +0530

    add static file

--- foodOnlin_main/settings.py
+++ foodOnlin_main/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'foodOnlin_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'foodOnlin_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'foodOnline',
+        'USER': 'Mysql',
+        'PASSWORD': '',
+        'HOST':'lacalhost',
     }
 }
 
@@ -116,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'foodOnlin_main/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
