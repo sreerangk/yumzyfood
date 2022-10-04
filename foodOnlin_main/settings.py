89333b3d86928e529e097ca7efa891025fc5f3bb
sreerang k <sreerangk77@gmail.com>
2022-10-04 19:59:45 +0530

    add .env

--- foodOnlin_main/settings.py
+++ foodOnlin_main/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k18wc)e%i(kbhm_$niom-y3ifp%5&_$$b@z_5p7+=b+5bx$xs_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'foodOnlin_main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'foodOnline',
-        'USER': 'root',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
         'PASSWORD': '',
-        'HOST':'localhost',
-        'PORT': '3306',
+        'HOST':config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
