b0116ee86385886bc381c12ff2b2797b341db7c5
sreerang k <sreerangk77@gmail.com>
2022-10-05 19:34:00 +0530

    add base

--- foodOnlin_main/settings.py
+++ foodOnlin_main/settings.py
@@ -127,6 +127,11 @@ STATICFILES_DIRS = [
     'foodOnlin_main/static'
 ]
 
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- foodOnlin_main/urls.py
+++ foodOnlin_main/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home"),
+    path('', views.home, name='home'),
+    path('accounts/',include('accounts.urls')),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
