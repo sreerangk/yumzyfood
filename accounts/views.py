f8da9b2c56f02c85a177ebe12c7f00d6cf2ba229
sreerang k <sreerangk77@gmail.com>
2022-10-06 10:04:19 +0530

    update registration

--- accounts/views.py
+++ accounts/views.py
@@ -3,19 +3,43 @@ from .forms import UserForm
 from .models import User
 
 # Create your views here.
+
 def registerUser(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.warning(request, 'You are already logged in!')
+        return redirect('dashboard')
+    elif request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
+            # Create the user using the form
+            # password = form.cleaned_data['password']
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # user.save()
+
+            # Create the user using create_user method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            user = form.save(commit=False)
-            user.set_password(password)
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
             user.save()
+
+            # Send verification email
+            mail_subject = 'Please activate your account'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
+            messages.success(request, 'Your account has been registered sucessfully!')
             return redirect('registerUser')
+        else:
+            print('invalid form')
+            print(form.errors)
     else:
-        form = UserForm(request.POST)
+        form = UserForm()
     context = {
         'form': form,
     }
-    return render(request, 'accounts/registerUser.html' ,context)
+    return render(request, 'accounts/registerUser.html', context)
