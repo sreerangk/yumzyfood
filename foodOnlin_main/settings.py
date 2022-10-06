a9acfb5f9313cb48f029cd6fe90c365b5698de0a
sreerang k <sreerangk77@gmail.com>
2022-10-06 20:00:32 +0530

    add forgotpassword

--- foodOnlin_main/settings.py
+++ foodOnlin_main/settings.py
@@ -145,4 +145,12 @@ MESSAGE_TAGS = {
 }
 
 
+# Email configuration
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+
+
 
