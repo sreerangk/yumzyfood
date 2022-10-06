7aea45ea425e900f5c3ac94cd684b2abbb258413
sreerang k <sreerangk77@gmail.com>
2022-10-06 14:23:54 +0530

    update view

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render,redirect
 
 from vendor.forms import VendorForm
 from .forms import UserForm
-from .models import User
+from .models import User,UserProfile
 from django.contrib import messages, auth
-
+from django.template.defaultfilters import slugify
 
 
 # Create your views here.
@@ -50,10 +50,47 @@ def registerUser(request):
     return render(request, 'accounts/registerUser.html', context)
 
 def registerVendor(request):
-    form = UserForm()
-    v_form = VendorForm()
+    if request.user.is_authenticated:
+        messages.warning(request, 'You are already logged in!')
+        return redirect('myAccount')
+    elif request.method == 'POST':
+        # store the data and create the user
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and v_form.is_valid:
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.VENDOR
+            user.save()
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            vendor_name = v_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+
+            # Send verification email
+            mail_subject = 'Please activate your account'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
+
+            messages.success(request, 'Your account has been registered sucessfully! Please wait for the approval.')
+            return redirect('registerVendor')
+        else:
+            print('invalid form')
+            print(form.errors)
+    else:
+        form = UserForm()
+        v_form = VendorForm()
+
     context = {
-        'form' : form,
-        'v_form' : v_form,
+        'form': form,
+        'v_form': v_form,
     }
+
     return render(request, 'accounts/registerVendor.html', context)
