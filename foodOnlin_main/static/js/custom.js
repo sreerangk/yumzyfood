2ae63647f4340f183aa1dd468f87eadad7803d20
sreerang k <sreerangk77@gmail.com>
2022-10-11 18:24:33 +0530

    add add_to_cart

--- foodOnlin_main/static/js/custom.js
+++ foodOnlin_main/static/js/custom.js
@@ -80,216 +80,25 @@ $(document).ready(function(){
         food_id = $(this).attr('data-id');
         url = $(this).attr('data-url');
         
-       
-        $.ajax({
+       data = {
+            food_id : food_id,
+       }
+       $.ajax({
             type: 'GET',
             url: url,
+            data: data,
             success: function(response){
-                console.log(response)
-                if(response.status == 'login_required'){
-                    swal(response.message, '', 'info').then(function(){
-                        window.location = '/login';
-                    })
-                }else if(response.status == 'Failed'){
-                    swal(response.message, '', 'error')
-                }else{
-                    $('#cart_counter').html(response.cart_counter['cart_count']);
-                    $('#qty-'+food_id).html(response.qty);
-
-                    // subtotal, tax and grand total
-                    applyCartAmounts(
-                        response.cart_amount['subtotal'],
-                        response.cart_amount['tax_dict'],
-                        response.cart_amount['grand_total']
-                    )
-                }
+                console.log(response.cart_counter['cart_count'])
+                $('#cart_counter').html(response.cart_counter['cart_count']);
+                $('#qty-'+food_id).html(response.qty);
             }
         })
+       
     })
-
-
-    // place the cart item quantity on load
+//place the cart item quantity on load
     $('.item_qty').each(function(){
         var the_id = $(this).attr('id')
         var qty = $(this).attr('data-qty')
         $('#'+the_id).html(qty)
     })
-
-    // decrease cart
-    $('.decrease_cart').on('click', function(e){
-        e.preventDefault();
-        
-        food_id = $(this).attr('data-id');
-        url = $(this).attr('data-url');
-        cart_id = $(this).attr('id');
-        
-        
-        $.ajax({
-            type: 'GET',
-            url: url,
-            success: function(response){
-                console.log(response)
-                if(response.status == 'login_required'){
-                    swal(response.message, '', 'info').then(function(){
-                        window.location = '/login';
-                    })
-                }else if(response.status == 'Failed'){
-                    swal(response.message, '', 'error')
-                }else{
-                    $('#cart_counter').html(response.cart_counter['cart_count']);
-                    $('#qty-'+food_id).html(response.qty);
-
-                    applyCartAmounts(
-                        response.cart_amount['subtotal'],
-                        response.cart_amount['tax_dict'],
-                        response.cart_amount['grand_total']
-                    )
-
-                    if(window.location.pathname == '/cart/'){
-                        removeCartItem(response.qty, cart_id);
-                        checkEmptyCart();
-                    }
-                    
-                } 
-            }
-        })
-    })
-
-
-    // DELETE CART ITEM
-    $('.delete_cart').on('click', function(e){
-        e.preventDefault();
-        
-        cart_id = $(this).attr('data-id');
-        url = $(this).attr('data-url');
-        
-        
-        $.ajax({
-            type: 'GET',
-            url: url,
-            success: function(response){
-                console.log(response)
-                if(response.status == 'Failed'){
-                    swal(response.message, '', 'error')
-                }else{
-                    $('#cart_counter').html(response.cart_counter['cart_count']);
-                    swal(response.status, response.message, "success")
-
-                    applyCartAmounts(
-                        response.cart_amount['subtotal'],
-                        response.cart_amount['tax_dict'],
-                        response.cart_amount['grand_total']
-                    )
-
-                    removeCartItem(0, cart_id);
-                    checkEmptyCart();
-                } 
-            }
-        })
-    })
-
-
-    // delete the cart element if the qty is 0
-    function removeCartItem(cartItemQty, cart_id){
-            if(cartItemQty <= 0){
-                // remove the cart item element
-                document.getElementById("cart-item-"+cart_id).remove()
-            }
-        
-    }
-
-    // Check if the cart is empty
-    function checkEmptyCart(){
-        var cart_counter = document.getElementById('cart_counter').innerHTML
-        if(cart_counter == 0){
-            document.getElementById("empty-cart").style.display = "block";
-        }
-    }
-
-
-    // apply cart amounts
-    function applyCartAmounts(subtotal, tax_dict, grand_total){
-        if(window.location.pathname == '/cart/'){
-            $('#subtotal').html(subtotal)
-            $('#total').html(grand_total)
-
-            console.log(tax_dict)
-            for(key1 in tax_dict){
-                console.log(tax_dict[key1])
-                for(key2 in tax_dict[key1]){
-                    // console.log(tax_dict[key1][key2])
-                    $('#tax-'+key1).html(tax_dict[key1][key2])
-                }
-            }
-        }
-    }
-
-    // ADD OPENING HOUR
-    $('.add_hour').on('click', function(e){
-        e.preventDefault();
-        var day = document.getElementById('id_day').value
-        var from_hour = document.getElementById('id_from_hour').value
-        var to_hour = document.getElementById('id_to_hour').value
-        var is_closed = document.getElementById('id_is_closed').checked
-        var csrf_token = $('input[name=csrfmiddlewaretoken]').val()
-        var url = document.getElementById('add_hour_url').value
-
-        console.log(day, from_hour, to_hour, is_closed, csrf_token)
-
-        if(is_closed){
-            is_closed = 'True'
-            condition = "day != ''"
-        }else{
-            is_closed = 'False'
-            condition = "day != '' && from_hour != '' && to_hour != ''"
-        }
-
-        if(eval(condition)){
-            $.ajax({
-                type: 'POST',
-                url: url,
-                data: {
-                    'day': day,
-                    'from_hour': from_hour,
-                    'to_hour': to_hour,
-                    'is_closed': is_closed,
-                    'csrfmiddlewaretoken': csrf_token,
-                },
-                success: function(response){
-                    if(response.status == 'success'){
-                        if(response.is_closed == 'Closed'){
-                            html = '<tr id="hour-'+response.id+'"><td><b>'+response.day+'</b></td><td>Closed</td><td><a href="#" class="remove_hour" data-url="/vendor/opening-hours/remove/'+response.id+'/">Remove</a></td></tr>';
-                        }else{
-                            html = '<tr id="hour-'+response.id+'"><td><b>'+response.day+'</b></td><td>'+response.from_hour+' - '+response.to_hour+'</td><td><a href="#" class="remove_hour" data-url="/vendor/opening-hours/remove/'+response.id+'/">Remove</a></td></tr>';
-                        }
-                        
-                        $(".opening_hours").append(html)
-                        document.getElementById("opening_hours").reset();
-                    }else{
-                        swal(response.message, '', "error")
-                    }
-                }
-            })
-        }else{
-            swal('Please fill all fields', '', 'info')
-        }
-    });
-
-    // REMOVE OPENING HOUR
-    $(document).on('click', '.remove_hour', function(e){
-        e.preventDefault();
-        url = $(this).attr('data-url');
-        
-        $.ajax({
-            type: 'GET',
-            url: url,
-            success: function(response){
-                if(response.status == 'success'){
-                    document.getElementById('hour-'+response.id).remove()
-                }
-            }
-        })
-    })
-
-   // document ready close 
 });
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, JsonResponse
 from django.shortcuts import render,get_object_or_404
+from .context_processors import get_cart_counter
 from vendor.models import Vendor
 from menu.models import Category, FoodItem 
 from django.db.models import Prefetch 
-
+from .models import Cart
 # Create your views here.
 
 def marketplace(request):
@@ -24,13 +25,36 @@ def vendor_detail(request, vendor_slug):
             queryset = FoodItem.objects.filter(is_available=True)
         )
     )
-    
+    if request.user.is_authenticated:
+        cart_item =Cart.objects.filter(user=request.user)
+    else:
+        cart_items = None
     context = {
         'vendor': vendor,
         'categories' : categories,
     }
     return render(request, 'marketplace/vendor_detail.html',context)
 
-
 def add_to_cart(request, food_id):
-    return HttpResponse('testing')
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            # Check if the food item exists
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                # Check if the user has already added that food to the cart
+                try:
+                    chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
+                    # Increase the cart quantity
+                    chkCart.quantity += 1
+                    chkCart.save()
+                    return JsonResponse({'status': 'Success', 'message': 'Increased the cart quantity', 'cart_counter': get_cart_counter(request), 'qty' : chkCart.quantity})
+                except:
+                    chkCart = Cart.objects.create(user=request.user, fooditem=fooditem, quantity=1)
+                    return JsonResponse({'status': 'Success', 'message': 'Added the food to the cart', 'cart_counter': get_cart_counter(request), 'qty' : chkCart.quantity})
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
+        
+    else:
+        return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
