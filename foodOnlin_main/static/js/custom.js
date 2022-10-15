05ae5f013f5b83b141cfa36ef9aa961a2621fc6d
sreerang k <sreerangk77@gmail.com>
2022-10-15 10:53:41 +0530

    add dynamic tax module

--- foodOnlin_main/static/js/custom.js
+++ foodOnlin_main/static/js/custom.js
@@ -102,7 +102,7 @@ $(document).ready(function(){
                     // subtotal , tax and grand total 
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'],
 
                     )
@@ -185,7 +185,7 @@ $(document).ready(function(){
                     // subtotal , tax and grand total 
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'],
                     )
                     removeCartItem(0, cart_id);
@@ -215,11 +215,19 @@ $(document).ready(function(){
     }
 
     // apply cart amounts 
-    function applyCartAmounts(subtotal, tax, grand_total){
+    function applyCartAmounts(subtotal, tax_dict, grand_total){
         if(window.location.pathname == '/cart/')
         $('#subtotal').html(subtotal)
-        $('#tax').html(tax)
         $('#total').html(grand_total)
+
+
+        
+        for(key1 in tax_dict){
+            for(key2 in tax_dict[key1]){
+
+                $('#tax-'+key1).html(tax_dict[key1][key2])
+            }
+        }
     }
  
     // Add oppening hour
--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,4 @@
+from unicodedata import decimal
 from django.db import models
 
 from accounts.models import User
@@ -13,3 +14,16 @@ class Cart(models.Model):
 
     def __unicode__(self):
         return self.user
+    
+    
+class Tax(models.Model):
+    tax_type = models.CharField(max_length=20, unique=True)
+    tax_percentage = models.DecimalField(decimal_places=2, max_digits=4, verbose_name='Tax percentage (%)')
+    is_active = models.BooleanField(default=True)
+    
+    
+    class Meta:
+        verbose_name_plural = 'tax'
+    
+    def __str__(self):
+        return self.tax_type
