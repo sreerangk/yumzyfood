3479151f0f1780b5982a55b2bc51a30d92ef19d3
sreerang k <sreerangk77@gmail.com>
2022-10-19 22:10:47 +0530

    add edit user in separate admin panel

--- customadmin/views.py
+++ customadmin/views.py
@@ -4,13 +4,13 @@ from django.shortcuts import render,redirect
 
 from django.contrib import messages, auth
 from accounts.models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def admin_login(request):
     if request.user.is_authenticated:
         return redirect('admin_index')
     else:
         if request.method == 'POST':
-            print('inside post')
             email = request.POST['email']
             password = request.POST['password']
             user = auth.authenticate(email=email, password=password)
@@ -28,42 +28,54 @@ def admin_login(request):
         else:
             
             return render(request, 'customadmin/admin_login.html')
-    #return render(request, 'customadmin/login.html')
+        #return render(request, 'customadmin/login.html')
 
-
+@login_required(login_url='login')
 def admin_index(request):
     if request.user.is_authenticated:
         user = User.objects.all()
         context = {'user':user}
         return render(request, 'customadmin/admin_index.html', context)
+    else:
+        return redirect('admin_login')
     
 
 
+@login_required(login_url='login')
 def admin_logout(request):
     auth.logout(request)
     return redirect('admin_login')
 
 
-def users(request):
+@login_required(login_url='login')
+def user_edit(request):
     user = User.objects.all()
-    context = {'user':user}
-    return render(request, 'customadmin/users.html', context)
+    context = {
+        'user':user
+        }
+    return render(request, 'customadmin/user_edit.html', context)
 
+@login_required(login_url='login')
 def blockuser(request,id):
     user = User.objects.get(id=id)
     user.is_active = False
     user.save()
-    return redirect('admin_index')
+    messages.success(request, 'blocked user successfully')
+    return redirect('user_edit')
 
 
+@login_required(login_url='login')
 def unblock(request,id):
     user = User.objects.get(id=id)
     user.is_active = True
     user.save()
-    return redirect('admin_index')
+    messages.success(request, 'unblocked user successfully')
+    return redirect('user_edit')
     
 
+@login_required(login_url='login')
 def deleteuser(request,id):
     user = User.objects.get(id=id)
     user.delete()
-    return redirect('admin_index')
+    messages.success(request, 'Delete user successfully')
+    return redirect('user_edit')
