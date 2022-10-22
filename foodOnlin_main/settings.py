56ff1bce616ec7d3dfdbf4a086a92b2ce77fc5dc
sreerang k <sreerangk77@gmail.com>
2022-10-22 14:49:10 +0530

    finish Razorpay pyment

--- foodOnlin_main/settings.py
+++ foodOnlin_main/settings.py
@@ -180,3 +180,7 @@ PAYPAL_CLIENT_ID = config('PAYPAL_CLIENT_ID')
 
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
+
+
+RZP_KEY_ID = config('RZP_KEY_ID')
+RZP_KEY_SECRET = config('RZP_KEY_SECRET')
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,7 @@
 from distutils.filelist import translate_pattern
 from http.client import HTTPResponse
 from multiprocessing import context
+from tokenize import Double
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
 from accounts.utils import send_notification
@@ -11,8 +12,17 @@ from .models import Order, OrderedFood, Payment
 import simplejson as json 
 from .utils import generate_order_number
 from django.contrib.auth.decorators import login_required
-
+import razorpay
+from foodOnlin_main.settings import RZP_KEY_ID,RZP_KEY_SECRET
 # Create your views here.
+
+
+
+
+client = razorpay.Client(auth=(RZP_KEY_ID, RZP_KEY_SECRET))
+
+
+
 
 @login_required(login_url='login')
 def place_order(request):
@@ -48,9 +58,27 @@ def place_order(request):
             order.save() #order id/pk is generated
             order.order_number = generate_order_number(order.id)
             order.save()
+            
+             # RazorPay Payment
+            DATA = {
+                "amount":float(order.total) * 100,
+                "currency": "INR",
+                "receipt": "receipt #"+order.order_number,
+                "notes": {
+                    "key1": "value3",
+                    "key2": "value2"
+                }
+            }
+            rzp_order = client.order.create(data=DATA)
+            rzp_order_id = rzp_order['id']
+            print(rzp_order)
+            
             context = {
                 'order' : order,
                 'cart_items': cart_items,
+                'rzp_order_id':rzp_order_id,
+                'RZP_KEY_ID': RZP_KEY_ID,
+                'rzp_amount': float(order.total) * 100,
             }
             return render(request, 'orders/place_order.html', context)
             
