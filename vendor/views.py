b26547301ee8f6ff34dccf26b08a80fcfbdfbacf
sreerang k <sreerangk77@gmail.com>
2022-10-23 12:23:25 +0530

    orders detail functionalites - custmr/vendor

--- vendor/views.py
+++ vendor/views.py
@@ -272,4 +272,11 @@ def order_detail(request,order_number):
         return redirect('vendor')
         
 def my_orders(request):
-    return render(request, 'vendors/my_orders.html')
+    vendor = Vendor.objects.get(user=request.user)
+    orders = Order.objects.filter(vendors__in=[vendor.id], is_ordered=True).order_by('created_at')
+    
+    context = {
+       'orders': orders,
+       
+    }
+    return render(request, 'vendor/my_orders.html', context)
