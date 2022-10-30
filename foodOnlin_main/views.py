ba37f45c06c02bba7fd963d48c7e0ce62d2d4f66
sreerang k <sreerangk77@gmail.com>
2022-10-30 11:18:37 +0530

    UPDATE  DELIVARY RADUS < 40

--- foodOnlin_main/views.py
+++ foodOnlin_main/views.py
@@ -29,7 +29,7 @@ def home(request):
 
         pnt = GEOSGeometry('POINT(%s %s)' % (get_or_set_current_location(request)))
 
-        vendors = Vendor.objects.filter(user_profile__location__distance_lte=(pnt, D(km=1000))).annotate(distance=Distance("user_profile__location", pnt)).order_by("distance")
+        vendors = Vendor.objects.filter(user_profile__location__distance_lte=(pnt, D(km=40))).annotate(distance=Distance("user_profile__location", pnt)).order_by("distance")
 
         for v in vendors:
             v.kms = round(v.distance.km, 1)
