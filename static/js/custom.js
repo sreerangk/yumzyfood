85de74958dc99fa4fbecd5da3916c90a4f21ff17
sreerang k <sreerangk77@gmail.com>
2022-11-05 12:11:20 +0530

    deploy to linode server

--- static/js/custom.js
+++ static/js/custom.js
@@ -80,14 +80,16 @@ $(document).ready(function(){
         food_id = $(this).attr('data-id');
         url = $(this).attr('data-url');
         
-       
+   
         $.ajax({
             type: 'GET',
             url: url,
+          
             success: function(response){
                 console.log(response)
                 if(response.status == 'login_required'){
-                    swal(response.message, '', 'info').then(function(){
+                    // alert message box
+                    swal(response.message, '','info').then(function(){
                         window.location = '/login';
                     })
                 }else if(response.status == 'Failed'){
@@ -95,27 +97,30 @@ $(document).ready(function(){
                 }else{
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     $('#qty-'+food_id).html(response.qty);
+                    
 
-                    // subtotal, tax and grand total
+                    // subtotal , tax and grand total 
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
                         response.cart_amount['tax_dict'],
-                        response.cart_amount['grand_total']
+                        response.cart_amount['grand_total'],
+
                     )
                 }
-            }
+            } 
         })
     })
 
-
-    // place the cart item quantity on load
+    
+    //place the cart item quantity on load
     $('.item_qty').each(function(){
         var the_id = $(this).attr('id')
         var qty = $(this).attr('data-qty')
         $('#'+the_id).html(qty)
     })
 
-    // decrease cart
+    
+    //decrease car
     $('.decrease_cart').on('click', function(e){
         e.preventDefault();
         
@@ -127,6 +132,7 @@ $(document).ready(function(){
         $.ajax({
             type: 'GET',
             url: url,
+            
             success: function(response){
                 console.log(response)
                 if(response.status == 'login_required'){
@@ -154,19 +160,21 @@ $(document).ready(function(){
             }
         })
     })
+    
+    //DELETE CART ITEM   
 
-
-    // DELETE CART ITEM
+       
     $('.delete_cart').on('click', function(e){
         e.preventDefault();
         
         cart_id = $(this).attr('data-id');
         url = $(this).attr('data-url');
         
-        
-        $.ajax({
+  
+       $.ajax({
             type: 'GET',
             url: url,
+        
             success: function(response){
                 console.log(response)
                 if(response.status == 'Failed'){
@@ -174,57 +182,57 @@ $(document).ready(function(){
                 }else{
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     swal(response.status, response.message, "success")
-
+                    
+                    // subtotal , tax and grand total 
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
                         response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
-
                     removeCartItem(0, cart_id);
                     checkEmptyCart();
-                } 
+                }
+        
             }
         })
     })
 
+    //delete the cart element if the qty is 0
 
-    // delete the cart element if the qty is 0
-    function removeCartItem(cartItemQty, cart_id){
-            if(cartItemQty <= 0){
-                // remove the cart item element
-                document.getElementById("cart-item-"+cart_id).remove()
-            }
-        
+    function removeCartItem(cartItemqty, cart_id){
+
+        if(cartItemqty <= 0){
+            //remove thecart item element
+            document.getElementById("cart-item-"+cart_id).remove()
+        }
     }
-
-    // Check if the cart is empty
+    //check if the cart is empty 
     function checkEmptyCart(){
         var cart_counter = document.getElementById('cart_counter').innerHTML
         if(cart_counter == 0){
-            document.getElementById("empty-cart").style.display = "block";
+            document.getElementById('empty-cart').style.display = "block";
+            
         }
     }
 
-
-    // apply cart amounts
+    // apply cart amounts 
     function applyCartAmounts(subtotal, tax_dict, grand_total){
-        if(window.location.pathname == '/cart/'){
-            $('#subtotal').html(subtotal)
-            $('#total').html(grand_total)
+        if(window.location.pathname == '/cart/')
+        $('#subtotal').html(subtotal)
+        $('#total').html(grand_total)
 
-            console.log(tax_dict)
-            for(key1 in tax_dict){
-                console.log(tax_dict[key1])
-                for(key2 in tax_dict[key1]){
-                    // console.log(tax_dict[key1][key2])
-                    $('#tax-'+key1).html(tax_dict[key1][key2])
-                }
+
+        
+        for(key1 in tax_dict){
+            for(key2 in tax_dict[key1]){
+
+                $('#tax-'+key1).html(tax_dict[key1][key2])
             }
         }
     }
+ 
+    // Add oppening hour
 
-    // ADD OPENING HOUR
     $('.add_hour').on('click', function(e){
         e.preventDefault();
         var day = document.getElementById('id_day').value
@@ -275,7 +283,9 @@ $(document).ready(function(){
         }
     });
 
-    // REMOVE OPENING HOUR
+
+    //remove opening hour
+
     $(document).on('click', '.remove_hour', function(e){
         e.preventDefault();
         url = $(this).attr('data-url');
@@ -291,5 +301,6 @@ $(document).ready(function(){
         })
     })
 
-   // document ready close 
+    // document ready close
 });
+ 
