550e705faa3e9e78c8b4edf352ad98065d29660e
sreerang k <sreerangk77@gmail.com>
2022-11-06 11:18:51 +0530

    run gdal path only on local server

--- foodOnlin_main/settings.py
+++ foodOnlin_main/settings.py
@@ -173,9 +173,11 @@ DEFAULT_FROM_EMAIL = 'Online Marketplace <helpa077637@gmail.com>'
 GOOGLE_API_KEY = config('GOOGLE_API_KEY')
 
 #GDALL PATH
-os.environ['PATH'] = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo') + ';' + os.environ['PATH']
-os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\data\proj') + ';' + os.environ['PATH']
-GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\gdal304.dll')
+
+if DEBUG == True:
+    os.environ['PATH'] = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo') + ';' + os.environ['PATH']
+    os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\data\proj') + ';' + os.environ['PATH']
+    GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\gdal304.dll')
 
 PAYPAL_CLIENT_ID = config('PAYPAL_CLIENT_ID')
 
