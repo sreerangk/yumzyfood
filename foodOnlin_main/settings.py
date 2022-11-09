296da226d5844de3ea8c342e17b47d7aa7f226da
sreerang k <sreerangk77@gmail.com>
2022-11-09 14:37:45 +0530

    hosting change

--- foodOnlin_main/settings.py
+++ foodOnlin_main/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
-from django import conf
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
