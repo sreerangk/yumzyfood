fcb93ee87c2f6f86759e2fc528dc1ff387cd7698
sreerang k <sreerangk77@gmail.com>
2022-11-10 14:31:14 +0530

    new changes to go live

--- foodOnlin_main/settings.py
+++ foodOnlin_main/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+from django import conf
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['172.105.33.160','127.0.0.1', 'yumzyfood.ml','www.yumzyfood.ml']
+ALLOWED_HOSTS = ['172.105.33.160', '127.0.0.1', 'yumzyfood.ml', 'www.yumzyfood.ml']
 
 
 # Application definition
